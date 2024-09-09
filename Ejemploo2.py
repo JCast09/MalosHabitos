0fd0667daf1eb06bbb6837cf310835d3db560217
77224027 <77224027@continental.edu.pe>
2024-09-09 18:37:22 -0500

    Refactorizacion ejem02

--- Ejemploo2.py
+++ Ejemploo2.py
@@ -1,12 +1,23 @@
-def calcular(a, b, c):
-    res = a * b + c
-    return res
 
-def principal():
-    x = 5
-    y = 3
-    z = 7
-    resultado = calcular(x, y, z)
+def suma(sumando1, sumando2):
+    suma = sumando1 + sumando2
+    return suma
+
+def multiplicacion(multiplicando, multiplicador):
+    producto = multiplicando * multiplicador
+    return producto
+
+
+if __name__ == "__main__":
+
+    sumando1 = float(input("Ingrese el Sumando 1: "))
+    sumando2 = float(input("Ingrese el Sumando 2: "))
+
+    sumaT = suma(sumando1, sumando2)
+
+    print(f"El multiplicador es: {sumaT}")
+
+    multiplicando = float(input("Ingrese el Multiplicando: "))
+    resultado = multiplicacion(sumaT, multiplicando)
     print("El resultado es:", resultado)
 
-principal()
